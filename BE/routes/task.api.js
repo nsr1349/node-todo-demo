297d911c38ec38fa48c0aab6ed65d6e001971db0
nsr1349 <79898508+nsr1349@users.noreply.github.com>
2024-10-16 14:39:12 +0900

    feat: 강의상 진도 구현

---
+++ BE/routes/task.api.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router()
+const taskController = require('../controller/task.controller')
+
+router.post('/', taskController.createTask)
+
+router.get('/', taskController.getTask)
+
+router.put('/:id', (req, res)=> {
+    res.send('update task')
+})
+
+router.delete('/:id', (req, res)=> {
+    res.send('del task')
+})
+
+module.exports = router
